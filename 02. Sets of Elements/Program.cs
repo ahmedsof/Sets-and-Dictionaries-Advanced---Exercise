e241bdf545d93ebb83cac70b6cb4d6d1dcefec9c
Ahmed Softev <ahmed_v@abv.bg>
2020-10-09 14:42:12 +0300

    C#

---
+++ 02. Sets of Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int m = input[1];
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                first.Add(num);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                second.Add(num);
+            }
+
+            HashSet<int> intersection = first.Intersect(second).ToHashSet();
+
+            Console.WriteLine(string.Join(" ", intersection));
+        }
+    }
+}
