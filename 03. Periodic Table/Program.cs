3c5396caeb5545aaae509ab1176c7d726adbd7f7
Ahmed Softev <ahmed_v@abv.bg>
2020-10-09 14:55:05 +0300

    C#

---
+++ 03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            HashSet<string> elements = new HashSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+
+                foreach (var item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            var sorted = elements.OrderBy(x => x);
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+    }
+}
---
+++ 04. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, int> ocurences = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (!ocurences.ContainsKey(currentNum))
+                {
+                    ocurences.Add(currentNum, 0);
+                }
+
+                ocurences[currentNum]++;
+            }
+
+            foreach (var item in ocurences)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
