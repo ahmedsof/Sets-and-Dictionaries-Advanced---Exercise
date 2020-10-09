ffb1a7d3634e1f62f01ebb4461861d5ce874fbe4
Ahmed Softev <ahmed_v@abv.bg>
2020-10-09 18:23:07 +0300

    C#

---
+++ 05. Count Symbols/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> occurences = new SortedDictionary<char, int>();
+
+            var input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if (!occurences.ContainsKey(current))
+                {
+                    occurences.Add(current, 0);
+                }
+
+                occurences[current]++;
+
+            }
+
+            foreach (var item in occurences)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+
+
+            
+        }
+    }
+}
---
+++ 06. Wardrobe/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace _06._Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" -> ").ToArray();
+                var color = input[0];
+                var clotes = input[1].Split(",").ToArray();
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                for (int j = 0; j < clotes.Length; j++)
+                {
+                    if (!wardrobe[color].ContainsKey(clotes[j]))
+                    {
+                        wardrobe[color].Add(clotes[j], 0);
+
+                    }
+
+                    wardrobe[color][clotes[j]]++;
+                }
+            }
+
+            var cloatingToFind = Console.ReadLine().Split(" ").ToArray();
+
+            foreach (var item in wardrobe)
+            {
+                Console.WriteLine($"{item.Key} clothes:");
+
+                foreach (var clothing in item.Value)
+                {
+                    Console.Write($"* {clothing.Key} - {clothing.Value}");
+
+                    if (clothing.Key == cloatingToFind[1] && item.Key == cloatingToFind[0])
+                    {
+                        Console.Write(" (found!)");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
